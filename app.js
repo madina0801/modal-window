4b47964afda3cd1516819c729ac7ab71ea236ff4
Madina <madinatussupova0820@gmail.com>
2022-06-07 14:33:19 +0600

    keypress event

--- app.js
+++ app.js
@@ -5,16 +5,15 @@ const overlay = document.querySelector(".overlay");
 const btnCloseModal = document.querySelector(".close-modal");
 const btnsOpenModal = document.querySelectorAll(".show-modal");
 
-const closeModal = function() {
-		modal.classList.add("hidden");
-		overlay.classList.add("hidden");
-}
+const closeModal = function () {
+  modal.classList.add("hidden");
+  overlay.classList.add("hidden");
+};
 
-const openModal = function() {
-	modal.classList.remove("hidden");
-	overlay.classList.remove("hidden");
-}
-
+const openModal = function () {
+  modal.classList.remove("hidden");
+  overlay.classList.remove("hidden");
+};
 
 for (let i = 0; i < btnsOpenModal.length; i++) {
   btnsOpenModal[i].addEventListener("click", openModal);
@@ -22,3 +21,9 @@ for (let i = 0; i < btnsOpenModal.length; i++) {
 
 btnCloseModal.addEventListener("click", closeModal);
 overlay.addEventListener("click", closeModal);
+
+document.addEventListener("keydown", function (e) {
+  if (e.key === "Escape" && !modal.classList.contains("hidden")) {
+    closeModal();
+  }
+});
